4fd4a30fd8b83fc551a6d29ca5c5e70839957341
Fred Hu <huyuqi0514@126.com>
2021-11-22 22:45:01 -0800

    brutal force update

--- README.md
+++ README.md
@@ -1,72 +1,13 @@
-# Deterministic Greedy Algo On Profit/Duration
+# Brutal Force DFS
 
 ## 1. Sample Test Result
 
-### - TOTAL TIME `1438`
-### - TOTAL PROFIT `2820.023124229029`
+### - TOTAL TIME : Still calculating...
+### - TOTAL PROFIT : Still calculating...
 
 ### - ID Order
-`
-60
-97
-57
-46
-44
-50
-99
-29
-69
-32
-71
-7
-61
-22
-53
-37
-24
-31
-87
-39
-8
-19
-6
-65
-72
-77
-34
-16
-63
-28
-38
-70
-67
-9
-17
-40
-30
-26
-20
-82
-83
-73
-62
-74
-21
-96
-42
-56
-25
-14
-68
-66
-86
-92
-43
-76
-33
-98
-`
+Result is in output file, the output name is the `count-profit number.out`.
 
 ## 2. Time & Space Complexity
-### - Time  `O(len(tasks)^2)`
+### - Time  `O(!len(tasks))`
 ### - Space `O(len(tasks))`
--- solver.py
+++ solver.py
@@ -1,48 +1,61 @@
 from parse import read_input_file, write_output_file
+import logging
 
-def solve(tasks):
-    """
-    Args:
-        tasks: list[Task], list of igloos to polish
-    Returns:
-        output: list of igloos in order of polishing
-    """
+log = "max-profit.log"
+logging.basicConfig(
+    filename=log,
+    level=logging.DEBUG,
+    format='%(asctime)s %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S',
+    )
+
+def dfs(tasks):
     N = len(tasks)
-    path = []
-    curTime = 0
-    for _ in range(N):
-        cur_task_quantity = len(tasks)
-        profit_over_duration_list = [0] * cur_task_quantity
-        greedy_index = 0
-        for i in range(cur_task_quantity):
-            profit_over_duration_list[i] = tasks[i].get_benefit_per_timestamp(curTime)
-            if profit_over_duration_list[greedy_index] < profit_over_duration_list[i]:
-                greedy_index = i
-        # Return if no valid moves
-        if profit_over_duration_list[greedy_index] < 0:
-            return path
-        # Pop this task
-        greedy_task = tasks.pop(greedy_index)
-        # Update path
-        path.append(greedy_task.get_task_id())
-        # Update curTime
-        curTime += greedy_task.get_duration()
+    count = 0
+    max_path = []
+    max_profit = 0
+    stack = []
+    for i in range(N, 0, -1):
+        stack.append([i])
+    while len(stack) > 0:
+        cur_path = stack.pop()
+        for i in range(1, N + 1):
+            if i not in cur_path:
+                stack.append(cur_path + [i])
+        cur_profit = get_total_benefit(tasks, cur_path)
+        if cur_profit > max_profit:
+            max_profit = cur_profit
+            max_path = cur_path
+            logging.info("New max profit " + str(max_profit))
+            write_output_file('./output/' + str(count) + "-" + str(cur_profit) + '.out', max_path)
+            logging.info('Solution in ./output/' + str(count) + "-" + str(cur_profit) + '.out')
+        if count % 100000 == 0:
+            logging.info("Iteration: " + str(count))
+        count += 1
+    return max_path
+
+
 
 def get_total_benefit(tasks, solution):
-    mapp = {}
     curTime = 0
     total_profit = 0
-    for task in tasks:
-        mapp[task.task_id] = task
     for s in solution:
-        total_profit += mapp[s].get_profit(curTime)
-        curTime += mapp[s].get_duration()
-    print(f"TOTAL TIME {curTime}")
-    print(f"TOTAL PROFIT {total_profit}")
+        total_profit += tasks[s - 1].get_profit(curTime)
+        curTime += tasks[s - 1].duration
+    return total_profit
 
+def show_total_benefit(tasks, solution):
+    curTime = 0
+    total_profit = 0
+    for s in solution:
+        total_profit += tasks[s-1].get_profit(curTime)
+        curTime += tasks[s-1].duration
+    logging.info(f"TOTAL TIME " + str(curTime))
+    logging.info(f"TOTAL PROFIT " + str(total_profit))
 
 if __name__ == '__main__':
     tasks = read_input_file('./test/100.in')
-    output = solve(tasks)
-    get_total_benefit(read_input_file('./test/100.in'), output)
+    tasks.sort(key=lambda x: x.task_id)
+    output = dfs(tasks)
+    show_total_benefit(read_input_file('./test/100.in'), output)
     write_output_file('./test/100.out', output)
