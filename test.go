19f7defb795407ce77918ae3051353212ea0bba8
Fred Hu <huyuqi0514@126.com>
2021-11-27 18:44:14 -0800

    temp

--- test.go
+++ test.go
@@ -1,34 +1,56 @@
-//package main
-//
-//import (
-//	Test "./test"
-//)
-//
-//func main(){
-//	//inputLengthStringList := []string{"small", "medium", "large"}
-//	//inputPath := "./input/"
-//	//for j := range inputLengthStringList {
-//	//	for i:=1; i<=300; i++ {
-//	//		fileName := inputPath + inputLengthStringList[j] + "/" + inputLengthStringList[j] + "-" + strconv.Itoa(i) + ".in"
-//	//		ttlist, err := Test.ReadInputFile(fileName)
-//	//		if err != nil {
-//	//			panic(err)
-//	//		}
-//	//		fmt.Println(ttlist)
-//	//	}
-//	//}
-//
-//	path := "./input/small/small-1.in"
-//	taskList, err := Test.ReadInputFile(path)
-//	if err != nil {
-//		panic(err)
-//	}
-//	outputList, err := Test.Solve(taskList)
-//	if err != nil {
-//		panic(err)
-//	}
-//	err = Test.WriteOutputFile("./shit.out", outputList)
-//	if err != nil {
-//		panic(err)
-//	}
-//}
+package main
+
+import (
+	Test "./test"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
+func init() {
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+	logFile, err := os.OpenFile("./test.log", os.O_CREATE | os.O_RDWR | os.O_APPEND,0644)
+	if err  != nil {
+		log.Panic("CANNOT OPEN LOG FILE")
+	}
+	log.SetOutput(logFile)
+}
+
+func main(){
+	rand.Seed(int64(2023))
+	path := []string{"small", "medium", "large"}
+	for index:=1; index<=300; index++ {
+		log.Printf("READY TO GO: ")
+		taskFileName := "./inputs/" + path[0] + "/" + path[0] + "-" +strconv.Itoa(index) + ".in"
+		rawOutputFileName := "./outputs/" + path[0] + "/" + path[0] + "-" +strconv.Itoa(index) + ".out"
+		newOutputFileName := "./newoutput/" + path[0] + "/" + path[0] + "-" +strconv.Itoa(index) + ".out"
+		log.Println("task file: " + taskFileName)
+		log.Println("raw output: " + rawOutputFileName)
+		log.Println("new output: " + newOutputFileName)
+
+		// Read in Tasks
+		taskListPTR, err := Test.ReadInputFile(taskFileName)
+		if err != nil {
+			panic(err)
+		}
+
+		// Read in Output
+		rawOPListPTR, err := Test.ReadOutputFile(rawOutputFileName)
+		if err != nil {
+			panic(err)
+		}
+
+		// Task into Solve
+		outputList, err := Test.Solve(taskListPTR, rawOPListPTR, 4)
+		if err != nil {
+			panic(err)
+		}
+
+		// Write the tasks order
+		err = Test.WriteOutputFile(newOutputFileName, outputList)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
